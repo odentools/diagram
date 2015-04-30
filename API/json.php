02e76d2f25aa50db8978d6567d8e57bdcdddc82f
Daiki Yano <ht12a092@gmail.com>
2015-05-01 01:38:31 +0900

    Add: Jsonクラス新規追加.

---
+++ API/json.php
@@ -0,0 +1,45 @@
+<?php
+
+class	JSON{
+
+	private		$result = NULL;
+	private		$StatusCode = NULL;
+
+	function	__construct() {
+
+		header("Content-Type: application/json; charset=utf-8");
+
+	}
+
+	function	__destruct() {
+		
+		$this->CheckStatus();
+
+		switch($this->StatusCode) {
+			
+			case '404':
+				$this->result[] = array("Status"=>array("message"=>"Error: Not found","code"=>"404"));
+				header("HTTP/1.1 404 Not Found");
+				break;
+
+			case '200':
+			default:
+				$this->result[] = array("Status"=>array("message"=>"Success: OK", "code"=>"200"));
+				break;
+
+		}
+
+		echo json_encode($this->result, JSON_UNESCAPED_UNICODE );
+
+	}
+
+
+	function	CheckStatus() {
+		
+		if(is_null($this->result)) $this->StatusCode = '404';
+
+	}
+
+}
+
+?>
