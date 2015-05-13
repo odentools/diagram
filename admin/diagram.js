e47edb0746b29eabee29386ff5bc5580c7b17916
yoshida <ht12a101@oecu.jp>
2015-05-13 23:06:53 +0900

    Change: インデントの調整

--- admin/diagram.js
+++ admin/diagram.js
@@ -11,16 +11,16 @@ jQuery(document).ready(function()
 
 	var departurePoint = 0;
 	var arrivalPoint = 0;
-	
+
 	var scrollOldXPoint = 0;
 	var scrollFlg = 0;
 
 	// キャンバスのイベントリスナー (クリック)
 	canvas.addEventListener('click', function(e){
-		
+
 		var expansion = parseFloat(document.diagram.expansion.value);
 		var offset = parseInt(document.diagram.offset.value) * expansion;
-		
+
 		var rect = e.target.getBoundingClientRect();
 		var mouseX = e.clientX - rect.left;
 		var mouseY = e.clientY - rect.top;
@@ -41,7 +41,7 @@ jQuery(document).ready(function()
 			}
 
 		}
-		
+
 		// 到着時刻が確定している場合
 		if(arrivalPoint != 0 || document.diagram.moveTime.value != 0) {
 
@@ -100,8 +100,7 @@ jQuery(document).ready(function()
 		}
 
 		repaint();
-
-    }, false);
+	}, false);
 
 
 	// キャンバスのイベントリスナー (マウス移動)
@@ -132,7 +131,7 @@ jQuery(document).ready(function()
 			if( scrollOldXPoint == 0) {
 				scrollOldXPoint = XPoint;
 			}
-			
+
 			document.diagram.offset.value = parseInt(document.diagram.offset.value) + scrollOldXPoint - XPoint;
 
 			repaint();
@@ -144,45 +143,45 @@ jQuery(document).ready(function()
 	}, false);
 
 
-    canvas.addEventListener("contextmenu", function(e){ e.preventDefault(); }, false);
+	canvas.addEventListener("contextmenu", function(e){ e.preventDefault(); }, false);
 
 
-    canvas.addEventListener('mousedown', function(e){
+	canvas.addEventListener('mousedown', function(e){
 
-        if(e.button == 0){
-            scrollFlg = 1;
-            scrollOldXPoint = 0;
-        }
-        if( e.button == 2){
-            DeparturePoint = 0;
-        }
-        repaint();
-    }, false);
-
-
-    canvas.addEventListener('mouseup', function(e){
-
-        if(e.button == 0){
-            scrollFlg = 0;
-        }
-        if( e.button == 2){
-            departurePoint = 0;
-        }
-        repaint();
-    }, false);
-
-
-    document.addEventListener('mouseup', function(e){
-
-        if(e.button == 0){
-            scrollFlg = 0;
-        }
-        if( e.button == 2){
-            departurePoint = 0;
-        }
+		if(e.button == 0){
+			scrollFlg = 1;
+			scrollOldXPoint = 0;
+		}
+		if( e.button == 2){
+			DeparturePoint = 0;
+		}
 		repaint();
-    }, false);
-	
+	}, false);
+
+
+	canvas.addEventListener('mouseup', function(e){
+
+		if(e.button == 0){
+			scrollFlg = 0;
+		}
+		if( e.button == 2){
+			departurePoint = 0;
+		}
+		repaint();
+	}, false);
+
+
+	document.addEventListener('mouseup', function(e){
+
+		if(e.button == 0){
+			scrollFlg = 0;
+		}
+		if( e.button == 2){
+			departurePoint = 0;
+		}
+			repaint();
+	}, false);
+
 	$("#deleteButton").click(function(){
 
 		// 選択されている行ID配列の取得
@@ -191,7 +190,7 @@ jQuery(document).ready(function()
 		if(arrrows.length == 0)  {
 			alert("削除する行を選択してください。");
 		} else {
-	
+
 			// 選択行の削除
 			// グリッドの下の方から削除していかないと、選択行すべて削除できない。
 			// BUG: http://www.trirand.com/blog/?page_id=393/bugs/delrowdata-bug-on-grid-with-multiselect
@@ -201,7 +200,7 @@ jQuery(document).ready(function()
 				$("#list").delRowData(arrrows[i]);
 
 		}
-		
+
 		return true;
 
 	});
@@ -375,7 +374,7 @@ function routeNameChange(obj) {
 			dataType: "json",
 
 			success: function(data, dataType) {
-				
+
 				data.RouteList.forEach(function(val) {
 
 					document.diagram.routeNameNew.value = val.RouteName;
@@ -383,10 +382,10 @@ function routeNameChange(obj) {
 					document.diagram.departureLocation.value = val.DepartureLocation;
 					document.diagram.arrivalLocation.value = val.ArrivalLocation;
 					document.diagram.diaNameNew.value = "";
-				
-				});	
-				
-		
+
+				});
+
+
 				// ダイアグループの(一覧)取得
 				$.ajax({
 
@@ -400,12 +399,12 @@ function routeNameChange(obj) {
 						$("#list").clearGridData();
 
 						data.DiaGroup.forEach(function(a) {
-			
+
 							var element = document.createElement("option");
 							element.value = a.id;
 							element.innerHTML = a.DiaName;
 							document.getElementById("diaName").appendChild(element);
-				
+
 						});
 
 					},
@@ -418,17 +417,17 @@ function routeNameChange(obj) {
 						} else {
 
 							alert("サーバーとの通信に失敗しました。\nCode: " + res.status);
-							
+
 						}
 						return false;
 
 					}
-			
+
 				});
 
 			},
 			error: function(res, textStatus, xhr) {
-				
+
 				if(res.status!=404) alert("サーバーとの通信に失敗しました。\nCode: " + res.status);
 				return false;
 
@@ -437,7 +436,7 @@ function routeNameChange(obj) {
 		});
 
 	} else {
-		
+
 		document.diagram.routeNameNew.value = "";
 		document.diagram.management.value = "";
 		document.diagram.departureLocation.value = "";
@@ -456,18 +455,18 @@ function routeNameChange(obj) {
 function diaNameChange(obj) {
 
 	if( obj.options[obj.selectedIndex].value > -1 ) {
-		
+
 		document.diagram.diaNameNew.value = obj.options[obj.selectedIndex].text;
 
 		$.ajax({
-			
+
 			type: "GET",
 			url: SERVER_APIURL+"Dia.json?DiaGroupT_ID_="+obj.options[obj.selectedIndex].value,
 			async: false, // ブラウザロック有効
 			dataType: "json",
 
 			success: function(data, dataType) {
-				
+
 				$("#list").clearGridData();
 
 				data.Dia.forEach(function(val) {
@@ -494,7 +493,7 @@ function diaNameChange(obj) {
 				}
 
 				return false;
-	
+
 			}
 
 		});
@@ -502,7 +501,7 @@ function diaNameChange(obj) {
 	} else {
 
 		document.diagram.diaNameNew.value = "";
-		
+
 	}
 
 	repaint();
@@ -549,7 +548,7 @@ function register() {
 	var postData = {'RouteListT':RouteListT, 'DiaGroupT':DiaGroupT, 'DiaT':DiaT};
 
 	$.ajax({
-		
+
 		type: "POST",
 		url: SERVER_URL+"db/T.php",
 		async: false, // ブラウザロック有効
@@ -574,17 +573,17 @@ function register() {
 /* ----------------------------------------
  * 重複を排除しながらpushする関数
  * ---------------------------------------- */
-function PushArray(array, value) {
+rfunction PushArray(array, value) {
 
 	// 存在しない場合、配列にpushする
 	if(! IsExists(array, value)) {
-		
+
 		array.push(value);
-		
+
 	}
-	
+
 	return true;
-	
+
 }
 
 /* ----------------------------------------
@@ -603,8 +602,8 @@ function IsExists(array, value) {
 		}
 
 	}
-	
+
 	// 存在しない場合falseを返す
 	return false;
-	
+
 }
