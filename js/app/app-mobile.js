1950c6c92ede128e5623d480938781ec3ed18b41
Masanori Ohgita <mp@ohgita.info>
2015-05-28 18:36:39 +0900

    Add: モバイル時刻表のリニューアル＆静的化
    * 現状で公開されているWebAPIへの対応 (日付指定対応を含む)
    * モバイル時刻表サイトのリニューアル＆静的化
    * これに伴うサイネージページの修正

---
+++ js/app/app-mobile.js
@@ -0,0 +1,139 @@
+/**
+	 AngularJS アプリケーションスクリプト
+**/
+
+/**
+	アプリケーションモジュール
+**/
+
+var app = angular.module('MobileApp', ['Diagram.services', 'ngRoute']);
+
+// ルーティング定義
+app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/routes', {
+				templateUrl: 'templates/mobile/page_routes.html',
+				controller: 'RoutesCtrl'
+			}).
+			when('/routes/:routeId', {
+				templateUrl: 'templates/mobile/page_timetable.html',
+				controller: 'TimetableCtrl'
+			}).
+			otherwise({
+				redirectTo: '/routes'
+			})
+		;
+	}
+]);
+
+// フィルタ定義: zpadding
+app.filter('zpadding', function() {
+	return function(num, digit) {
+		while (num.toString().length < digit) {
+			num = '0' + num.toString();
+		}
+		return num;
+	};
+});
+
+// フィルタ定義: substring
+app.filter('substring', function() {
+	return function(str, start, end) {
+		if (str == null) {
+			return null;
+		}
+		return str.substring(start, end);
+	};
+})
+
+/**
+	路線一覧ページ用コントローラ
+**/
+app.controller('RoutesCtrl', function($scope, Routes, Helpers) {
+	$scope.routes = {};
+
+	Routes.fetchAll(function(list){
+		$scope.routes = list;
+	});
+});
+
+/**
+	時刻表ページ用コントローラ
+**/
+app.controller('TimetableCtrl', function($scope, $routeParams, $timeout, Routes, Timetable, Helpers) {
+	// 全便の配列
+	var allBuses = [];
+	// 将来便の配列
+	$scope.buses = [];
+	// 次便
+	$scope.nextBus = null;
+	// 選択された路線
+	$scope.routeId = $routeParams.routeId; // URLから路線IDを取得
+	$scope.route = null;
+
+	// 路線情報および時刻表の取得
+	$scope.fetchTimetable = function(route_id) {
+		// 全便の配列を初期化
+		allBuses = [];
+		// 路線情報を取得
+		Routes.fetch(
+			route_id,
+			function(route) { // 路線情報を取得できた時
+				$scope.route = route;
+				if (route == null) {
+					window.alert("当該路線は存在しません");
+					return;
+				}
+
+				// 時刻表を取得
+				Timetable.fetch(
+					route_id,
+					new Date(),
+					function(buses) {
+						// 全便の配列を書き換え
+						allBuses = buses;
+						// 将来便の配列を更新
+						$scope.updateBuses();
+					},
+					function(data, status){
+						window.alert("[エラー] 時刻表を取得できません\n" + data.status.message);
+					}
+				);
+			},
+			function(data, status) { // エラー時
+				window.alert("[エラー] 路線情報を取得できません\n" + data.status.message);
+			}
+		);
+	};
+
+	// 将来の便をフィルタリング
+	$scope.updateBuses = function() {
+		if (allBuses == null) {
+			return
+		}
+
+		// 将来便の配列を書き換え
+		$scope.buses = Timetable.filterPresentBuses(allBuses);
+		// 次便を抽出
+		if (0 < $scope.buses.length) {
+			$scope.nextBus = $scope.buses[0];
+		} else {
+			$scope.nextBus = null;
+		}
+	};
+
+	/*----*/
+
+	// 時刻表を取得
+	$scope.fetchTimetable($scope.routeId);
+
+	// 更新タイマーを開始
+	var func_update = function() {
+		// 次便および残りの便を更新
+		$scope.updateBuses();
+		// タイマーを再始動
+		$timeout(func_update, 1000);
+	};
+	$timeout(func_update, 1000);
+});
--- js/app/services-diagram.js
+++ js/app/services-diagram.js
@@ -4,97 +4,6 @@
 
 var services = angular.module('Diagram.services', ['ngResource']);
 
-// 路線リストサービス
-services.factory('Routes', ['$http', function($http) {
-	var service = {
-		// 路線リストの取得
-		fetchAll: function(opt_id, callback, opt_err_callback) {
-			// リクエスト
-			$http.get('http://oecu.pw/api/1/RouteList.json')
-				.success(function(data, status, headers, config) {
-					var list = data.RouteList;
-					(callback(list));
-				})
-				.error(function(data, status, headers, config) {
-					if (opt_err_callback != null) {
-						(opt_err_callback(data));
-					}
-				});
-		},
-
-		// 路線の取得
-		fetch: function(route_id, callback, opt_err_callback) {
-			// 指定された路線IDの路線をリクエスト
-			$http.get('http://oecu.pw/api/1/RouteList.json?id=' + route_id)
-				.success(function(data, status, headers, config) {
-					if (data.RouteList == null || data.RouteList.length <= 0) {
-						(callback(null));
-						return;
-					}
-
-					// 今日の曜日を調べる
-					var now = new Date();
-					var is_holiday = false;
-					if (now.getDay == 0 | now.getDay == 6) {
-						is_holiday = true;
-					}
-
-					// 当該路線IDに属するダイアグループのリストをリクエスト
-					var route = data.RouteList[0];
-					$http.get('http://oecu.pw/api/1/DiaGroup.json?RouteListT_ID_=' + route_id)
-						.success(function(data, status, headers, config) {
-							// 今日のダイアグループを選択
-							var today_group = null;
-							for (var i = 0, l = data.DiaGroup.length; i < l; i++) {
-								var group = data.DiaGroup[i];
-								if ((group.DiaName == "平日" && !is_holiday) || group.DiaName == "休日") {
-									today_group = group;
-									break;
-								}
-							}
-							// 路線とダイアグループを結合して返す
-							if (today_group != null) {
-								$.extend(route, today_group);
-							}
-							(callback(route));
-						})
-						.error(function(data, status, headers, config) {
-							if (opt_err_callback != null) {
-								(opt_err_callback(data));
-							}
-						});
-				})
-				.error(function(data, status, headers, config) {
-					if (opt_err_callback != null) {
-						(opt_err_callback(data));
-					}
-				});
-		}
-	};
-	return service;
-}]);
-
-// サービス定義: ダイアグラム リスト
-services.factory('Diagrams', ['$http', function($http) {
-	var service = {
-		// ダイアグラムの取得
-		fetch: function(dia_group_id, callback, opt_err_callback) {
-			// リクエスト
-			$http.get('http://oecu.pw/api/1/Dia.json?DiaGroupT_ID_=' + dia_group_id)
-				.success(function(data, status, headers, config) {
-					var diagrams = data.Dia;
-					(callback(diagrams));
-				})
-				.error(function(data, status, headers, config) {
-					if (opt_err_callback != null) {
-						(opt_err_callback(data));
-					}
-				});
-		}
-	};
-	return service;
-}]);
-
 // ヘルパサービス
 services.factory('Helpers', [function() {
 	var service = {
@@ -132,6 +41,183 @@ services.factory('Helpers', [function() {
 		// ゼロ埋め
 		zpadding: function(num, digit) {
 			return ("00" + num).substr(-digit);
+		}
+	};
+	return service;
+}]);
+
+// 路線リストサービス
+services.factory('Routes', ['$http', function($http) {
+	var service = {
+		// 路線リストの取得
+		fetchAll: function(callback, opt_err_callback) {
+			// リクエスト
+			$http.get('http://oecu.pw/api/1.1/RouteList.json')
+				.success(function(data, status, headers, config) {
+					var list = [];
+					/* 路線リストAPIが何故か連想配列で返ってきて、キーの数値とIDが不一致なので配列へ変換する */
+					for (id in data.RouteList) {
+						var r = data.RouteList[id];
+						// テーマカラーを設定
+						r.color = service.getRouteColor(r);
+						// 配列へ追加
+						list.push(r);
+					}
+					// コールバックを実行
+					(callback(list));
+				})
+				.error(function(data, status, headers, config) {
+					if (opt_err_callback != null) {
+						(opt_err_callback(data, status));
+					}
+				});
+		},
+
+		// 路線の取得
+		fetch: function(route_id, callback, opt_err_callback) {
+			// 指定された路線IDの路線をリクエスト
+			$http.get('http://oecu.pw/api/1.2/RouteList.json?id=' + route_id)
+				.success(function(data, status, headers, config) {
+					if (data.RouteList == null || data.RouteList.length <= 0) {
+						(callback(null));
+						return;
+					}
+
+					// 今日の曜日を調べる
+					var now = new Date();
+					var is_holiday = false;
+					if (now.getDay == 0 | now.getDay == 6) {
+						is_holiday = true;
+					}
+
+					// 当該路線を取得
+					var route = data.RouteList[0];
+					// テーマカラーを設定
+					route.color = service.getRouteColor(route);
+
+					// 当該路線IDに属するダイアグループのリストをリクエスト
+					$http.get('http://oecu.pw/api/1/DiaGroup.json?RouteListT_ID_=' + route_id)
+						.success(function(data, status, headers, config) {
+							// 今日のダイアグループを選択
+							var today_group = null;
+							for (var i = 0, l = data.DiaGroup.length; i < l; i++) {
+								var group = data.DiaGroup[i];
+								if ((group.DiaName == "平日" && !is_holiday) || group.DiaName == "休日") {
+									today_group = group;
+									break;
+								}
+							}
+							// 路線とダイアグループを結合して返す
+							if (today_group != null) {
+								$.extend(route, today_group);
+							}
+							(callback(route));
+						})
+						.error(function(data, status, headers, config) {
+							if (opt_err_callback != null) {
+								(opt_err_callback(data, status));
+							}
+						});
+				})
+				.error(function(data, status, headers, config) {
+					if (opt_err_callback != null) {
+						(opt_err_callback(data, status));
+					}
+				});
+		},
+
+		// 路線のテーマカラーの取得
+		getRouteColor: function(route) {
+			if (route.Management == "大阪電気通信大学") {
+				return "#599900";
+			} else if (route.Management == "近鉄バス") {
+				return "#ffff00";
+			}
+			return "#eeeeee";
+		}
+	};
+	return service;
+}]);
+
+// サービス定義: 時刻表
+services.factory('Timetable', ['$http', 'Helpers', function($http, Helpers) {
+	var service = {
+		// 時刻表の取得
+		fetch: function(dia_id, date, callback, opt_err_callback) {
+			// リクエスト
+			var date_str = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
+			$http.get('http://oecu.pw/api/1.2.1/Dia.json?route_id=' + dia_id + '&date=' + date_str)
+				.success(function(data, status, headers, config) {
+					var buses = [];
+					for (id in data.Dia) {
+						var item = data.Dia[id];
+						// IDをオブジェクトへ挿入
+						item.Id = parseInt(id);
+						// 発着時間をDateオブジェクトへ変換
+						item.ArrivaDate = Helpers.timeStrToDate(item.ArrivalTime);
+						item.DepartureDate = Helpers.timeStrToDate(item.DepartureTime);
+						// 備考の文字列を処理
+						if (item.Note.length == 0) {
+							item.Note = null;
+						}
+						// 配列へ挿入
+						buses.push(item);
+					}
+					// コールバックを実行
+					(callback(buses));
+				})
+				.error(function(data, status, headers, config) {
+					if (opt_err_callback != null) {
+						(opt_err_callback(data, status));
+					}
+				});
+		},
+
+		// 将来の便のみに絞り込む
+		filterPresentBuses: function(buses) {
+			var now = new Date();
+			var filtered_buses = [];
+			var next_bus = null;
+
+			for (var i = 0, l = buses.length; i < l; i++) {
+				var bus = buses[i];
+
+				// 過ぎた便であるかどうか
+				if (now.getDate() != bus.DepartureDate.getDate()) { // 明日の便
+					bus.IsPast = true;
+				} else if (bus.DepartureDate < now) { // 今日の過ぎた便
+					bus.IsPast = true;
+					// 明日の便にする
+					bus.DepartureDate.setDate(bus.DepartureDate.getDate() + 1);
+				} else { // 過ぎていない便
+					bus.IsPast = false;
+				}
+
+				// 次の便であるかどうか
+				if (!bus.IsPast && next_bus == null) {
+					// 残り時間を計算
+					var rem_msec = Helpers.getRemainMiliSecByDate(bus.DepartureDate);
+					bus.RemainDateStr = Helpers.miliSecToTimeStr(rem_msec, false);
+					// 次の便として保持
+					next_bus = bus;
+					bus.IsNext = true;
+					// まもなく出発するかどうか
+					bus.IsSoon = false;
+					if (rem_msec <= 60000) { // 60秒以内ならば
+						bus.IsSoon = true;
+					}
+				} else {
+					bus.RemainDateStr = null;
+					bus.IsNext = false;
+					bus.IsSoon = false;
+				}
+
+				if (!bus.IsPast) {
+					filtered_buses.push(bus);
+				}
+			}
+
+			return filtered_buses;
 		}
 	};
 	return service;
